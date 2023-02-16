a61abf7a6e42eb685612aebcb845b1ee74284ac9
Sidaoui <mssidaoui@gmail.com>
2023-02-16 11:32:50 +0100

    projectSkeleton

---
+++ mobilityBackend/src/main/java/com/gurus/mobility/MobilityApplication.java
@@ -0,0 +1,13 @@
+package com.gurus.mobility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilityApplication.class, args);
+	}
+
+}
---
+++ mobilityBackend/src/test/java/com/gurus/mobility/MobilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gurus.mobility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
