6223e6a0d9de6f36f849dc166d0b999a38dc7c50
ramez-wechteti <ramez.wechteti@esprit.tn>
2023-02-16 14:11:38 +0100

    First creation of entities

---
+++ mobilityBackend/src/main/java/com/gurus/mobility/entity/ForumChat/Comment.java
@@ -0,0 +1,31 @@
+package com.gurus.mobility.entity.ForumChat;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id_cmt", nullable = false)
+    private Long idCmt;
+
+    @Column(name = "up_vote_cmt")
+    private Long upVoteCmt;
+
+    @Column(name = "content_cmt")
+    private String contentCmt;
+
+    @Column(name = "creation_date_cmt")
+    private LocalDate creationDateCmt;
+
+
+}
