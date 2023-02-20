99566fec86095fbb3d2dd6bc776a99bcf920a73c
BargaouiIkram123 <98560268+BargaouiIkram123@users.noreply.github.com>
2023-02-20 15:59:43 +0100

    Ikram_entities (#15)
    * adding_entities_ahmed
    
    * adding entities
    
    * adding service candidacy
    
    * adding service of candidacy
    
    * adding service of candidacy
    
    ---------
    
    Co-authored-by: Ahmed <ahmed.balti@esprit.tn>
    Co-authored-by: Gh1ndri <66020672+Gh1ndri@users.noreply.github.com>

--- mobilityBackend/src/main/java/com/gurus/mobility/entity/Candidacy/Result.java
+++ mobilityBackend/src/main/java/com/gurus/mobility/entity/Candidacy/Result.java
@@ -1,4 +1,4 @@
-package com.gurus.mobility.entity;
+package com.gurus.mobility.entity.Candidacy;
 
 import lombok.*;
 
---
+++ mobilityBackend/src/main/java/com/gurus/mobility/repository/Candidacy/IResultRepository.java
@@ -0,0 +1,7 @@
+package com.gurus.mobility.repository.Candidacy;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IResultRepository {
+}
