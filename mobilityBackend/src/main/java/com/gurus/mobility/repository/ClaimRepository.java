2f7fcca5edb7d6b98135716239dd4d5f5b588b3c
Ahmed <ahmed.balti@esprit.tn>
2023-02-20 15:22:00 +0100

    adding service

--- mobilityBackend/src/main/java/com/gurus/mobility/repository/ClaimRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gurus.mobility.repository;
-
-import com.gurus.mobility.entity.claim.Claim;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClaimRepository extends JpaRepository<Claim, Long> {
-}
--- mobilityBackend/src/main/java/com/gurus/mobility/repository/Commentaire/ICommentaireRepository.java
+++ mobilityBackend/src/main/java/com/gurus/mobility/repository/Commentaire/ICommentaireRepository.java
@@ -1,8 +1,9 @@
 package com.gurus.mobility.repository.Commentaire;
 
+import com.gurus.mobility.entity.Offer.Commentaire;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ICommentaireRepository extends JpaRepository<Offer,Integer> {
+public interface ICommentaireRepository extends JpaRepository<Commentaire,Integer> {
 }
