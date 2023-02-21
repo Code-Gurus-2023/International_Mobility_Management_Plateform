5bebdb992c52584970309080be5f9d57da5424b3
Ahmed <ahmed.balti@esprit.tn>
2023-02-21 22:13:40 +0100

    adding entities

--- mobilityBackend/src/main/java/com/gurus/mobility/controller/OfferRestController.java
+++ mobilityBackend/src/main/java/com/gurus/mobility/controller/OfferRestController.java
@@ -1,2 +1,46 @@
-package com.gurus.mobility.controller;public class OfferRestController {
+package com.gurus.mobility.controller;
+
+import com.gurus.mobility.entity.Offer.Offer;
+import com.gurus.mobility.service.OfferService.IOfferService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/offers")
+public class OfferRestController {
+
+    @Autowired
+    private IOfferService offerService;
+
+    @GetMapping
+    public List<Offer> getAllOffers() {
+        return offerService.getAllOffers();
+    }
+
+    @GetMapping("/{id}")
+    public Offer getOfferById(@PathVariable Integer id) {
+        return offerService.getOfferById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<Offer> createOffer(@Valid @RequestBody Offer offer) {
+        Offer createdOffer = offerService.createOffer(offer);
+        return new ResponseEntity<>(createdOffer, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/modify-offer")
+    public Offer modifyOffer(@RequestBody Offer o) {
+        return offerService.updateOffer(o);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOffer(@PathVariable Integer id) {
+        offerService.deleteOffer(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- mobilityBackend/src/main/java/com/gurus/mobility/repository/OfferRepository/IOfferRepository.java
+++ mobilityBackend/src/main/java/com/gurus/mobility/repository/OfferRepository/IOfferRepository.java
@@ -1,2 +1,9 @@
-package com.gurus.mobility.repository.OfferRepository;public interface IOfferRepository {
+package com.gurus.mobility.repository.OfferRepository;
+
+import com.gurus.mobility.entity.Offer.Offer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IOfferRepository extends JpaRepository<Offer,Integer> {
 }
