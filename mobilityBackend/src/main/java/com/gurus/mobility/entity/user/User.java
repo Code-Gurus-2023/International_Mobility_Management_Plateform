dbbd38f337eebf95e2c8ef2fcb99ac6912d2c3f9
Gh1ndri <66020672+Gh1ndri@users.noreply.github.com>
2023-02-28 21:55:14 +0100

    Association entities (#22)
    * change gitignore file
    
    * delete properties file
    
    * switch to main and pull amine and nadine entities
    
    * update properties with adding bezkoder
    
    * add relations between entities
    
    * change gitignore file
    
    * switch to main and pull amine and nadine entities
    
    * add relations between entities
    
    ---------
    
    Co-authored-by: ramez-wechteti <ramez.wechteti@esprit.tn>

--- mobilityBackend/src/main/java/com/gurus/mobility/entity/user/User.java
+++ mobilityBackend/src/main/java/com/gurus/mobility/entity/user/User.java
@@ -1,14 +1,26 @@
 package com.gurus.mobility.entity.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.gurus.mobility.entity.Accomodation.Accomodation;
+import com.gurus.mobility.entity.Accomodation.Reservation;
+import com.gurus.mobility.entity.Candidacy.Candidacy;
+import com.gurus.mobility.entity.Candidacy.Result;
+import com.gurus.mobility.entity.ForumChat.ChatRoom;
+import com.gurus.mobility.entity.ForumChat.Comment;
+import com.gurus.mobility.entity.ForumChat.Discussion;
+import com.gurus.mobility.entity.ForumChat.Notification;
+import com.gurus.mobility.entity.Offer.Commentaire;
+import com.gurus.mobility.entity.Offer.Offer;
+import com.gurus.mobility.entity.alert.Alert;
+import com.gurus.mobility.entity.claim.Claim;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Size;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -58,7 +70,162 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "user_id")
+    private Set<Alert> alerts = new LinkedHashSet<>();
+
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "user_id")
+    private Set<Claim> claims = new LinkedHashSet<>();
+
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    @OneToOne(orphanRemoval = true)
+    @JoinColumn(name = "reservation_id_res", unique = true)
+    private Reservation reservation;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Accomodation> accomodations;
+
+    @OneToMany(mappedBy = "user", orphanRemoval = true)
+    private Set<Candidacy> candidacies = new LinkedHashSet<>();
+
+    @OneToOne(mappedBy = "user", orphanRemoval = true)
+    private Result result;
+
+    @OneToMany(mappedBy = "user", orphanRemoval = true)
+    private Set<Discussion> discussions = new LinkedHashSet<>();
+
+    @OneToMany(mappedBy = "user", orphanRemoval = true)
+    private Set<Comment> comments = new LinkedHashSet<>();
+
+    @OneToMany(mappedBy = "user", orphanRemoval = true)
+    private Set<ChatRoom> chatRooms = new LinkedHashSet<>();
+
+    @OneToMany(mappedBy = "user", orphanRemoval = true)
+    private Set<Notification> notifications = new LinkedHashSet<>();
+
+    @OneToMany(mappedBy = "user", orphanRemoval = true)
+    private Set<Offer> offers = new LinkedHashSet<>();
+
+    @OneToMany(mappedBy = "user", orphanRemoval = true)
+    private Set<Commentaire> commentaires = new LinkedHashSet<>();
+
+    public Set<Commentaire> getCommentaires() {
+        return commentaires;
+    }
+
+    public void setCommentaires(Set<Commentaire> commentaires) {
+        this.commentaires = commentaires;
+    }
+
+    public Set<Offer> getOffers() {
+        return offers;
+    }
+
+    public void setOffers(Set<Offer> offers) {
+        this.offers = offers;
+    }
+
+    public Set<Notification> getNotifications() {
+        return notifications;
+    }
+
+    public void setNotifications(Set<Notification> notifications) {
+        this.notifications = notifications;
+    }
+
+    public Set<ChatRoom> getChatRooms() {
+        return chatRooms;
+    }
+
+    public void setChatRooms(Set<ChatRoom> chatRooms) {
+        this.chatRooms = chatRooms;
+    }
+
+    public Set<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(Set<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public Set<Discussion> getDiscussions() {
+        return discussions;
+    }
+
+    public void setDiscussions(Set<Discussion> discussions) {
+        this.discussions = discussions;
+    }
+
+    public Result getResult() {
+        return result;
+    }
+
+    public void setResult(Result result) {
+        this.result = result;
+    }
+
+    public Set<Candidacy> getCandidacies() {
+        return candidacies;
+    }
+
+    public void setCandidacies(Set<Candidacy> candidacies) {
+        this.candidacies = candidacies;
+    }
+
+    public Set<Claim> getClaims() {
+        return claims;
+    }
+
+    public void setClaims(Set<Claim> claims) {
+        this.claims = claims;
+    }
+
+    public Set<Alert> getAlerts() {
+        return alerts;
+    }
+
+    public void setAlerts(Set<Alert> alerts) {
+        this.alerts = alerts;
+    }
+
+    public Reservation getReservation() {
+        return reservation;
+    }
+
+    public void setReservation(Reservation reservation) {
+        this.reservation = reservation;
+    }
+
+    public Set<Accomodation> getAccomodations() {
+        return accomodations;
+    }
+
+    public void setAccomodations(Set<Accomodation> accomodations) {
+        this.accomodations = accomodations;
+    }
+
+
     public User(String username, String email, String encode) {
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+        return id != null && Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
