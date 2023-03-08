536f5f4f81b44b4820e7ba428da8ea8e10f26354
ramez-wechteti <ramez.wechteti@esprit.tn>
2023-03-08 20:00:56 +0100

    Adding scheduled method

--- mobilityBackend/src/main/java/com/gurus/mobility/repository/ForumChatRepos/ICommentRepository.java
+++ mobilityBackend/src/main/java/com/gurus/mobility/repository/ForumChatRepos/ICommentRepository.java
@@ -2,9 +2,20 @@ package com.gurus.mobility.repository.ForumChatRepos;
 
 import com.gurus.mobility.entity.ForumChat.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface ICommentRepository extends JpaRepository<Comment, Long> {
+
+//    @Query("SELECT c FROM Comment c WHERE DATE(c.creationDateCmt) = DATE(:date) - INTERVAL 1 DAY")
+//    List<Comment> getCommentsByCreationDateCmtIs(LocalDateTime date);
+
+    @Query("SELECT c FROM Comment c WHERE c.creationDateCmt BETWEEN :startDate AND :endDate")
+    List<Comment> getCommentsByCreationDateCmtIs(LocalDateTime startDate, LocalDateTime endDate);
+
 }
 
--- mobilityBackend/src/main/java/com/gurus/mobility/service/ForumChatService/CommentServiceImpl.java
+++ mobilityBackend/src/main/java/com/gurus/mobility/service/ForumChatService/CommentServiceImpl.java
@@ -1,12 +1,65 @@
 package com.gurus.mobility.service.ForumChatService;
 
+import com.gurus.mobility.entity.ForumChat.Comment;
 import com.gurus.mobility.repository.ForumChatRepos.ICommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Service
 public class CommentServiceImpl implements ICommentService {
 
     @Autowired
     ICommentRepository commentRepository;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+
+    @Scheduled(cron = "0 0 23 * * *")
+    public void exportComments() throws IOException {
+        String fileName = "commentsLog.txt";
+//        String filePath = "src/main/resources/" + fileName;
+//        Resource resource = resourceLoader.getResource("file:" + filePath);
+        Path path = Paths.get("src", "main", "resources", fileName);
+
+        LocalDate today = LocalDate.now();
+        LocalDateTime startOfDay = today.atStartOfDay();
+        LocalDateTime endOfDay = today.atTime(LocalTime.MAX);
+        List<Comment> comments = commentRepository.getCommentsByCreationDateCmtIs(startOfDay, endOfDay);
+
+        String content = "";
+
+        for (Comment c: comments) {
+            content +=
+                    c.getDiscussion().getNameDsc()
+                    + ": " +c.getCreationDateCmt().toString()
+                    + ": " + c.getContentCmt()
+                    + ": " + c.getUpVoteCmt()
+                    + ": " + c.getUser().userName + "\n"
+            ;
+        }
+
+        //Path path = Paths.get(resource.getURI());
+        if (!Files.exists(path)) {
+            Files.createDirectories(path.getParent());
+            Files.createFile(path);
+        }
+
+        Files.writeString(path, content, StandardOpenOption.APPEND);
+
+    }
 }
