790bb591cbfa6e0f251078fca03754f82eef0fe9
Sidaoui <mssidaoui@gmail.com>
2023-04-25 21:41:36 +0100

    sidaoui_all_back

--- mobilityBackend/src/main/java/com/gurus/mobility/service/AccomodationServices/IReservationService.java
+++ mobilityBackend/src/main/java/com/gurus/mobility/service/AccomodationServices/IReservationService.java
@@ -1,14 +1,20 @@
 package com.gurus.mobility.service.AccomodationServices;
 
 
+import com.gurus.mobility.entity.Accomodation.Reservation;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Map;
+
 public interface IReservationService {
 
 
-    /**public Reservation saveReservation(Reservation reservation);
+    public Reservation AffectPayementAndSaveReservation(Reservation reservation,Long idPayement);
     public List<Reservation> getReservation();
     public ResponseEntity<Reservation> getReservationById(Long idRes);
     public ResponseEntity<Reservation> updateReservation(Long idRes, Reservation reservation);
-    public ResponseEntity<Map<String,Boolean>> deleteReservation(Long idRes);**/
+    public ResponseEntity<Map<String,Boolean>> deleteReservation(Long idRes);
 
 
 }
--- mobilityBackend/src/main/java/com/gurus/mobility/service/AccomodationServices/ReservationServiceImpl.java
+++ mobilityBackend/src/main/java/com/gurus/mobility/service/AccomodationServices/ReservationServiceImpl.java
@@ -1,10 +1,54 @@
 package com.gurus.mobility.service.AccomodationServices;
 
 
+import com.gurus.mobility.entity.Accomodation.Payement;
+import com.gurus.mobility.entity.Accomodation.Reservation;
+import com.gurus.mobility.repository.AccomodationRepository.PayementRepository;
+import com.gurus.mobility.repository.AccomodationRepository.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.webjars.NotFoundException;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ReservationServiceImpl implements IReservationService{
 
+    @Autowired
+    private ReservationRepository reservationRepository;
+    @Autowired
+    private PayementRepository payementRepository;
 
+    @Override
+    public Reservation AffectPayementAndSaveReservation(Reservation reservation,Long idPayement) {
+        Reservation reservation1=reservationRepository.save(reservation);
+        Payement payement=payementRepository.findById(idPayement)
+                .orElseThrow(()->new NotFoundException("We didnt found the payement"));
+                reservation1.setPayement(payement);
+               return reservationRepository.save(reservation1);
+    }
+
+    @Override
+    public List<Reservation> getReservation() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Reservation> getReservationById(Long idRes) {
+        Reservation reservation=reservationRepository.findById(idRes)
+                .orElseThrow(()->new NotFoundException("We didn't found the the reservation"));
+        return ResponseEntity.ok(reservation);
+    }
+
+    @Override
+    public ResponseEntity<Reservation> updateReservation(Long idRes, Reservation reservation) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Map<String, Boolean>> deleteReservation(Long idRes) {
+        return null;
+    }
 }
