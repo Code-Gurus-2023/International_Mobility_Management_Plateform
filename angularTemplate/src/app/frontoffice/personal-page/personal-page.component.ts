92d3b2e4ceb96045eb69d32a879a2d2794c49d83
gh1ndri <leith.ghandri@gmail.com>
2023-05-06 23:55:16 +0100

    new template

---
+++ angularTemplate/src/app/frontoffice/personal-page/personal-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-page',
+  templateUrl: './personal-page.component.html',
+  styleUrls: ['./personal-page.component.css']
+})
+export class PersonalPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
